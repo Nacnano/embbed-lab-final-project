e95114ade5b49c2b14b930602082f3f582950e7e
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-05-14 14:51:19 +0700

    feat: connect to rtdb

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -7,22 +7,42 @@ import StatusBar from "@/components/StatusBar";
 import { Info } from "@/types/Info";
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue, set } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
 const Home = () => {
-  const [data, setData] = useState<Info>();
+  const [brightness, setBrightness] = useState<number>();
+  const [distance, setDistance] = useState<number>();
+
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
 
   useEffect(() => {
-    // TODO : add database connection instead of mock data
-    const mockData = {
-      brightness: 80,
-      distance: 2,
+    const Ref = ref(database, "sensor");
+
+    const unsub1 = onValue(Ref, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+      setBrightness(data.brightness);
+      setDistance(data.distance);
+    });
+    return () => {
+      unsub1();
     };
-    setData(mockData);
   }, []);
 
-  const luminosty = data
-    ? data.brightness * (4 * Math.PI * data.distance ** 2)
-    : -1;
+  const luminosty =
+    brightness && distance ? brightness * (4 * Math.PI * distance ** 2) : -1;
 
   return (
     <div className="flex flex-col container mx-auto px-4 py-8 items-center">
@@ -31,19 +51,19 @@ const Home = () => {
       </h1>
       {/* <StatusBar /> */}
 
-      {data ? (
+      {brightness && distance ? (
         <div className="flex flex-col justify-center items-center gap-5">
           <StatusCard value={luminosty} />
           <div className="flex flex-row p-5 justify-center items-center gap-5 bg-gray-300 rounded-lg">
-            <InfoCard type={InfoType.Distance} value={data.distance} />
-            <InfoCard type={InfoType.Luminosity} value={data.brightness} />
+            <InfoCard type={InfoType.Distance} value={distance} />
+            <InfoCard type={InfoType.Luminosity} value={brightness} />
           </div>
         </div>
       ) : (
         <LoadingSpinner />
       )}
 
-      <div className="flex flex-col py-5 items-center">
+      {/* <div className="flex flex-col py-5 items-center">
         Gen Phy 2 goes bruhhhh
         <Image
           src={"/lumens-vs-lux-diagram.jpg"}
@@ -51,7 +71,7 @@ const Home = () => {
           width={400}
           height={800}
         />
-      </div>
+      </div> */}
     </div>
   );
 };
