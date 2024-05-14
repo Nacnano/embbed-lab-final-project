74551e01e620b78f3f14a86c7e6a6a56c1b849db
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-05-14 20:28:06 +0700

    feat: handle out of range distance

--- web/src/components/InfoCard.tsx
+++ web/src/components/InfoCard.tsx
@@ -12,22 +12,27 @@ export default function InfoCard({ type, value }: InfoCardProps) {
   let title = "";
   let unit = "";
   let color = "";
+  let processedValue = "";
 
   switch (type) {
     case InfoType.Distance:
       title = "Distance";
       unit = "cm";
       color = "blue-500";
+      processedValue =
+        2 <= value && value <= 800 ? String(value) : "Out of range";
       break;
     case InfoType.Luminosity:
       title = "Brightness";
       unit = "lux";
       color = "blue-500";
+      processedValue = String(value);
       break;
     default:
       title = "untitled";
       unit = "unknown";
       color = "gray-500";
+      processedValue = String(value);
       break;
   }
 
@@ -37,12 +42,20 @@ export default function InfoCard({ type, value }: InfoCardProps) {
       style={{ boxShadow: "6px 6px 10px -1px rgba(0,0,0,0.15)" }}
     >
       <div className="text-center font-bold text-lg">{title}</div>
-      <div className={`text-center font-extrabold text-4xl text-${color}`}>
-        {value}
-      </div>
-      <div className={`text-center font-bold text-3xl text-${color}`}>
-        {unit}
-      </div>
+      {processedValue == String(value) ? (
+        <div>
+          <div className={`text-center font-extrabold text-4xl text-${color}`}>
+            {processedValue}
+          </div>
+          <div className={`text-center font-bold text-3xl text-${color}`}>
+            {unit}
+          </div>
+        </div>
+      ) : (
+        <div className={`text-center font-extrabold text-2xl text-${color}`}>
+          {processedValue}
+        </div>
+      )}
     </div>
   );
 }
